8730d0cdaef006c80e57c592658654171109ea87
OrdosX <i@ordosx.tech>
2021-11-14 22:15:07 +0800

    添加：视频列表内一大节只显示一个链接

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         延河课堂播放增强
 // @namespace    https://www.ordosx.tech/
-// @version      1.4
-// @description  点击视频空白处切换播放暂停，左右方向键控制5秒快进快退，上下方向键切换2倍速和原速，数字键1和2切换单双屏
+// @version      1.5
+// @description  视频列表内一大节只显示一个链接，点击视频空白处切换播放暂停，左右方向键控制5秒快进快退，上下方向键切换2倍速和原速，数字键1和2切换单双屏
 // @author       OrdosX
 // @match        https://www.yanhekt.cn/session/*
 // @icon         https://www.google.com/s2/favicons?domain=yanhekt.cn
@@ -14,11 +14,13 @@
   window.addEventListener('load', () => {
     main()
   })
-  const main = () => {
-    if (!document.querySelector('.player-panel')) {
-      setTimeout(main, 20)
-      return
+  async function waitUntilExist (selectors) {
+    while (!document.querySelector(selectors)) {
+      await (new Promise(resolve => setTimeout(resolve, 20)))
     }
+  }
+  async function main () {
+    await waitUntilExist('.player-panel')
     const playerPanel = document.querySelector('.player-panel')
     const mainPlayerVideo = document.querySelector('.main-player video')
     const playerMask = document.querySelector('.player-mask')
@@ -35,30 +37,47 @@
       document.querySelector('.playback-rates-text').innerText = `${mainPlayerVideo.playbackRate.toFixed(1)}X`
     }
     document.onkeydown = (e) => {
-      e.preventDefault()
       switch (e.key) {
         case 'ArrowLeft': // 左方向键快退五秒
+          e.preventDefault()
           mainPlayerVideo.currentTime -= 5
           break
         case 'ArrowRight': // 右方向键快进5秒
+          e.preventDefault()
           mainPlayerVideo.currentTime += 5
           break
         case 'ArrowUp': // 上方向键2倍速
+          e.preventDefault()
           document.querySelectorAll('video').forEach((e) => { e.playbackRate = 2 })
           break
         case 'ArrowDown': // 下方向键原速
+          e.preventDefault()
           document.querySelectorAll('video').forEach((e) => { e.playbackRate = 1 })
           break
         case ' ': // 空格暂停
+          e.preventDefault()
           pauseButton.click()
           break
         case '1': // 切换单屏（摄像头画面）
+          e.preventDefault()
           singleScreen.click()
           break
         case '2': // 切换双屏（摄像头画面和PPT）
+          e.preventDefault()
           dualScreen.click()
           break
       }
     }
+    await waitUntilExist('.video-list-content li')
+    document.querySelectorAll('.video-list-content li').forEach(e => {
+      if (!e.nextSibling) return
+      const [week, day, rawPeriod] = e.textContent.split(' ')
+      const [sWeek, sDay, sRawPeriod] = e.nextSibling.textContent.split(' ')
+      const period = parseInt(rawPeriod.match(/[0-9]+/)[0])
+      const sPeriod = parseInt(sRawPeriod.match(/[0-9]+/)[0])
+      if (week === sWeek && day === sDay && period - sPeriod === 1) {
+        e.hidden = true
+      }
+    })
   }
 })()
